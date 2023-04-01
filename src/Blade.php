831c39a32472797059381eac10c3e2c79d5595bd
Sergio Compean <scompean24@gmail.com>
2023-04-01 19:50:57 -0400

    cache flag value

--- src/Blade.php
+++ src/Blade.php
@@ -55,7 +55,7 @@ class Blade
     /**
      * Whether compiled views should be cached into directory.
      */
-    protected static bool $cacheCompiled = false;
+    protected static bool $cacheCompiled = true;
 
     /**
      * The file compiler instance.
